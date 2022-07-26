e96aebf62a8951455cba7683f2a2aeb226bd7df1
dhoine345 <lue42@yandex.ru>
2022-07-26 16:57:40 +0300

    add users controllers

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+mongoose.connect('mongodb://localhost:27017/moviesdb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
