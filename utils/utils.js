461889c16ba32b16656b95b5e17895b1f749bf6e
dhoine345 <lue42@yandex.ru>
2022-07-26 18:42:35 +0300

    add utils for requests

--- utils/utils.js
+++ utils/utils.js
@@ -1,9 +1,29 @@
 const { regexUrl } = require('./constants');
+const { resCodes, messages } = require('./constants');
+const NotFoundError = require('./errors/NotFoundError');
+const BadRequestError = require('./errors/BadRequestError');
 
 function validateUrl(v) {
   return regexUrl.test(v);
 }
 
+function handleErrors(err, next) {
+  if (err.name === 'CastError' || err.name === 'ValidationError') {
+    next(new BadRequestError(messages.badRequest));
+    return;
+  }
+  next(err);
+}
+
+function handleRequest(item, res, message) {
+  if (!item) {
+    throw new NotFoundError(message);
+  }
+  res.status(resCodes.OK_CODE).send({ data: item });
+}
+
 module.exports = {
   validateUrl,
+  handleErrors,
+  handleRequest,
 };
