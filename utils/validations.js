9db58eeb8c8cceb997a33439c6ad5eedc428d2f4
dhoine345 <lue42@yandex.ru>
2022-07-27 08:52:52 +0300

    add validation.js

---
+++ utils/validations.js
@@ -0,0 +1,67 @@
+const { celebrate, Joi } = require('celebrate');
+const { regexUrl } = require('./constants');
+
+/*const validateLogin = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+});*/
+
+/*const validateRegistration = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(regexUrl),
+  }),
+});*/
+
+/*const validateGetUserById = celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().required().length(24).hex(),
+  }),
+});*/
+
+const validateUpdateProfile = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
+  }),
+});
+
+/*const validateUpdateAvatar = celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required().pattern(regexUrl),
+  }),
+});*/
+
+/*const validateCreateCard = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().pattern(regexUrl),
+  }),
+});*/
+
+/*const validateDeleteCard = celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().required().length(24).hex(),
+  }),
+});*/
+
+/*const validateSetLike = celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().required().length(24).hex(),
+  }),
+});*/
+
+/*const validateRemoveLike = celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().required().length(24).hex(),
+  }),
+});*/
+
+module.exports = {
+  validateUpdateProfile,
+};
