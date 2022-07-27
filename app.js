c3b28f0e2c06f41d8253932a958e3f6f3fb0baf9
dhoine345 <lue42@yandex.ru>
2022-07-27 09:15:32 +0300

    add routes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
 
@@ -9,6 +10,8 @@ mongoose.connect('mongodb://localhost:27017/moviesdb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
