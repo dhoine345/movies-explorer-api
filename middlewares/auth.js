981d6808a1076dfe8ecf5ec655aebe724fb4951b
dhoine345 <lue42@yandex.ru>
2022-07-27 09:19:01 +0300

    add auth.js

---
+++ middlewares/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const { messages } = require('../utils/constants');
+const UnauthorizedError = require('../utils/errors/UnauthorizedError');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new UnauthorizedError(messages.unauthorized);
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, NODE_ENV ? JWT_SECRET : 'dev-secret');
+  } catch (err) {
+    next(new UnauthorizedError(messages.unauthorized));
+    return;
+  }
+
+  req.user = payload;
+  next();
+};
