21e90444bd3a0653c6aa49b5050fddbe24a0c851
dhoine345 <lue42@yandex.ru>
2022-07-27 21:41:08 +0300

    fix

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -1,10 +1,10 @@
 const rateLimit = require('express-rate-limit');
-const { message } = require('../utils/constants');
+//const { message } = require('../utils/constants');
 
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hour
   max: 100,
-  message: message.tooMuchRequestsErr,
+  //message: message.tooMuchRequestsErr,
   standardHeaders: true,
   legacyHeaders: false,
 });
