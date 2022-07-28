96f0520cf0dd7b91909b791963aa230429887f8d
dhoine345 <lue42@yandex.ru>
2022-07-28 18:10:59 +0300

    fix error in var name

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -5,7 +5,7 @@ module.exports = ((err, req, res, next) => {
   res
     .status(statusCode)
     .send({
-      message: statusCode === resCodes.INTERVAL_SERVER_ERROR
+      message: statusCode === resCodes.INTERNAL_SERVER_ERROR
         ? messages.serverError
         : message,
     });
